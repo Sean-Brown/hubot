f6325901efbf1441cb432ed84d8b8977e2c33053
Keith Olenchak <kenjin.domini@gmail.com>
2015-10-17 17:58:51 -0400

    base version

--- scripts/exclamation.js
+++ scripts/exclamation.js
@@ -2,30 +2,30 @@
  module
  */
 /**
- # Description
- #   Detects '!!!' and returns a random exclamation from google
- #
- # Dependencies:
- #   none
- #
- # Configuration:
- #   none
- #
- # Commands:
- #   <!!!> - <returns a random exclamation from google>
- #
- # Notes:
- #   none
- #
- # Author:
- #   Keith Olenchak
+  Description
+    Detects '!!!' and returns a random exclamation from google
+
+ Dependencies:
+    none
+
+ Configuration:
+   none
+
+ Commands:
+   <!!!> - <returns a random exclamation from google>
+
+ Notes:
+   none
+
+ Author:
+   Keith Olenchak
  */
 /**
  *
  * @param robot
  */
 module.exports = function (robot) {
-  robot.hear('/!!!/i', function (response) {
+  robot.hear(/!!!/i, function (response) {
     response.send('I\'m sooooo excited!!');
     // TODO: call google API.
   });
