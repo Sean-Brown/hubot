1e9ac5897f6bea6669c9b1babbece5128718ee5e
Keith Olenchak <kenjin.domini@gmail.com>
2015-10-17 12:55:06 +0000

    modified to no longer create the hubot as the package should handle it now.

--- prereqs.sh
+++ prereqs.sh
@@ -5,11 +5,10 @@ sudo apt-get install -y build-essential libssl-dev git python2.7
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 echo "source ~/.nvm/nvm.sh" >> ~/.profile
 source ~/.profile
-nvm install 4.1.1
-nvm alias default 4.1.1
-npm install -g hubot yo generator-hubot
+nvm install 4.2.1
+nvm alias default 4.2.1
 mkdir ~/bot
 git clone https://github.com/kenjindomini/hubot.git ~/bot
 cd ~/bot
-yo hubot
 sudo wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+npm install
